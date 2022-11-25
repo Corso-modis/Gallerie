fe93ffbba166c637a9c4a5d7a6cd9f6840535697
Pablo Gnoato <pablo2gnoato@gmail.com>
2022-11-25 14:27:20 +0100

    Correzzioni globali a repo, service e control

--- src/main/java/com/modis/service/ImmagineService.java
+++ src/main/java/com/modis/service/ImmagineService.java
@@ -2,6 +2,8 @@ package com.modis.service;
 
 import java.util.List;
 
+import com.modis.entities.Immagine;
+
 public interface ImmagineService {
 	public void save(Immagine immagine);
 	public List<Immagine> findAll();
