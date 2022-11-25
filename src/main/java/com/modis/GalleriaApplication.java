0207d07b7dda06fec2d307cc4b9cb4ee53f01491
KalashniRob <robert.c.matei@outlook.com>
2022-11-25 10:37:30 +0100

    Scheletro progetto

---
+++ src/main/java/com/modis/GalleriaApplication.java
@@ -0,0 +1,13 @@
+package com.modis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalleriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modis/GalleriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
